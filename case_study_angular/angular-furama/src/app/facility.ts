4e42144697092b74e90cc23e8f09cba06316884a
lehuutai0310 <lehuutai0310@gmail.com>
2023-03-10 16:31:59 +0700

    case_study_angular

---
+++ case_study_angular/angular-furama/src/app/facility.ts
@@ -0,0 +1,14 @@
+export interface Facility {
+  name?: string;
+  area?: number;
+  cost?: number;
+  maxPeople?: number;
+  standardRoom?: string;
+  description?: string;
+  poolArea?: number;
+  numberOfFloors?: number;
+  facilityFree?: string;
+  rentType?: string;
+  facilityType?: string;
+  img?: string;
+}
